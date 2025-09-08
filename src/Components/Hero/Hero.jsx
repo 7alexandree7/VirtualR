39941f7a0d97b3a385561b63ab9dd71973dbc8cc
alexandre <alexandre020602@gmail.com>
2025-09-08 19:18:34 -0300

    mudança na nomeclatura de asstes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
-import video1 from '../../Assets/video1.mp4'
-import video2 from '../../Assets/video2.mp4'
+import video1 from '../../assets/video1.mp4'
+import video2 from '../../assets/video2.mp4'
 
 const Hero = () => {
     return (
